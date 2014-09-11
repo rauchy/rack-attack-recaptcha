f2f3ea4d43532c64147d39e873e818f0fa130416
Omer Rauchwerger <omer@rauchy.net>
2014-09-11 05:43:12 +0300

    default all helper options to an empty hash

--- lib/rack/attack/recaptcha/verification_helper.rb
+++ lib/rack/attack/recaptcha/verification_helper.rb
@@ -2,7 +2,7 @@ module Rack
   module Attack
     module Recaptcha
       module VerificationHelper
-        def verify_recaptcha_if_under_attack(options)
+        def verify_recaptcha_if_under_attack(options={})
           if request.env["rack.attack.use_recaptcha"]
             verify_recaptcha(options)
           else
