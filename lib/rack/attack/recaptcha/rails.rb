75e34e78f2d1bbf63d27ddfa0af9e7784048b0af
Omer Rauchwerger <omer@rauchy.net>
2014-09-13 14:25:16 +0300

    avoid plugging into Rails when running tests

--- lib/rack/attack/recaptcha/rails.rb
+++ lib/rack/attack/recaptcha/rails.rb
@@ -1,4 +1,6 @@
-require "recaptcha/rails"
+if defined?(Rails)
+  require "recaptcha/rails"
 
-ActionView::Base.send(:include, Rack::Attack::Recaptcha::ClientHelper)
-ActionController::Base.send(:include, Rack::Attack::Recaptcha::VerificationHelper)
+  ActionView::Base.send(:include, Rack::Attack::Recaptcha::ClientHelper)
+  ActionController::Base.send(:include, Rack::Attack::Recaptcha::VerificationHelper)
+end
