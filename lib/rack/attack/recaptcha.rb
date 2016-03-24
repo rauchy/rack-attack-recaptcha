f5be30201e8a11617b021b33e0248a858e1ff525
Zach Kemp <zvkemp@gmail.com>
2016-03-24 08:31:18 -0700

    move middleware logic to own class

--- lib/rack/attack/recaptcha.rb
+++ lib/rack/attack/recaptcha.rb
@@ -3,29 +3,14 @@ require "rack/attack/recaptcha/version"
 require "rack/attack/recaptcha/client_helper"
 require "rack/attack/recaptcha/verification_helper"
 require "rack/attack/recaptcha/rails"
+require "rack/attack/recaptcha/middleware"
 
 module Rack
   class Attack
     module Recaptcha
       class << self
         def new(app)
-          @default_response = Rack::Attack.throttled_response
-          @rack_attack = Rack::Attack.new(app).tap { |attack|
-            attack.class.throttled_response = lambda { |env|
-              if env["rack.attack.match_type"] == :recaptcha
-                env["rack.attack.use_recaptcha"] = true
-                app
-              else
-                @default_response
-              end.call(env)
-            }
-          }
-
-          self
-        end
-
-        def call(env)
-          @rack_attack.call(env)
+          Rack::Attack::Recaptcha::Middleware.new(app)
         end
       end
     end
